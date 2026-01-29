7c22143a76af0f57e7ff68bb5b830dc0ab103569
Daswealth-Okikiola <adenijimykhael113@gmail.com>
2026-01-29 21:37:46 +0000

    feat: learnt how to convert Celsius to Fahrenheit using variable and string concatenation

---
+++ celsius.js
@@ -0,0 +1,16 @@
+// Convert Celsius to Fahrenheit
+// To convert Celsius to Fahrenheit, you can use the following formula:
+
+
+// F = C×1.8 + 32
+
+// Directions:
+// Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12°C.
+
+//     NOTE: "12°C" reads as "12 degrees Celsius".
+
+var celsius = 12;
+var farenheit = celsius * 1.8 + 32;
+
+console.log("12 degree Celsius in Fahrenheit is" + farenheit);
+
