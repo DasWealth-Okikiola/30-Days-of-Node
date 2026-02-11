90010a7d36b7f099263a98f90df127047f258766
Daswealth-Okikiola <adenijimykhael113@gmail.com>
2026-02-11 08:48:49 +0000

    feat: Created a breakfast method with different data types

---
+++ menuItemMethod.js
@@ -0,0 +1,17 @@
+// Directions:
+// Create a breakfast object to represent the following menu item:
+
+// The Lumberjack - $9.95
+// eggs, sausage, toast, hashbrowns, pancakes
+// The object should contain properties for the name, price, and ingredients.
+
+// Your Code:
+
+var breakfast = {
+    name: "Lumberjack",
+    price: "$9.95",
+    ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+}
+
+console.log(breakfast.ingredients)
+
